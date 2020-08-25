078587b6d47c9c27c0f1e73e7ba13ada2d297316
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-08-25 13:12:24 +0000

    Writting the code for uploading image for add_book page

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 import os
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 from flask_pymongo import PyMongo
+import uuid
 
 app = Flask(__name__)
 
 app.config['MONGO_DBNAME'] = 'dcdmpDB'
 app.config['MONGO_URI'] = 'mongodb+srv://dcdmpDBUser:PassWord@dcdmp.cs4wp.mongodb.net/dcdmpDB?retryWrites=true&w=majority'
-
+app.secret_key = 'some secret kay'
 mongo = PyMongo(app)
 
 @app.route('/')
@@ -47,6 +48,75 @@ def auth():
 @app.route('/single_book_page')
 def single_book_page():
     return render_template("single_book_page.html")    
+
+
+
+################ INSERTING IMAGE BOOK ###############
+
+# Checking if file has a extension which is in allowed extensions list
+def allowed_filesieze(filesize):
+
+    if int(filesize) <= 0.5 * 1024 * 1024:
+        return True
+    else:
+        return False    
+
+
+# Checking if image has allowed extension
+def allowed_ext(filename):
+ 
+    ext = filename.rsplit('.',1)[1]
+    allowed_ext = ['JPG', 'JPEG', 'PNG', 'gif']
+
+    if ext.upper() in allowed_ext:
+        return ext
+    else:
+        return False    
+
+
+# Generating random filename
+def generate_random_img_name():
+    r_name = str(uuid.uuid4())
+    return r_name
+
+
+# Uploading image
+def upload_image():
+
+    image = request.files['file']
+    # Checking image's settings
+    if image.filename == '':
+        flash('The image must have a name')
+        redirect(request.url)
+        return False
+
+    if not allowed_filesieze(request.cookies.get('filesize')):
+        flash("The allowed maximum image size is 500 KB ")
+        redirect(request.url)
+        return False
+
+    if not allowed_ext(image.filename):
+        flash("The allowed extensions are PNG, JPG, JPEG, GIF")  
+        redirect(request.url)
+        return False  
+    # Saving image to folder
+    img_folder_path = 'static/img/uploaded'
+    r_f_name = generate_random_img_name() + '.' + allowed_ext(image.filename)
+    image.save(os.path.join(img_folder_path, r_f_name))
+
+    
+
+# Insertting image
+@app.route('/insert_book', methods=['POST'])
+def insert_book():
+
+    if request.method == "POST":
+       
+        upload_image()
+
+
+    return redirect(url_for('add_book'))
+
 
 
 if __name__ == '__main__':
