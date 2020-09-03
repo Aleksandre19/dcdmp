00d5f7f034e5e0278721ba3928a31a9d8f6d1009
Aleksandre19 <aleksandre.voronini@gmail.com>
2020-09-03 21:27:26 +0000

    Finishing Project

--- app.py
+++ app.py
@@ -11,18 +11,15 @@ if path.exists('env.py'):
     import env
 
 
-
 app = Flask(__name__)
+
 
 app.config['MONGO_DBNAME'] = os.environ.get('MONGO_DBNAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.secret_key = os.environ.get('SECRET_KEY')
 
 
-
-
 mongo = PyMongo(app)
-
 
 
 def data_for_search_autocomplete():
